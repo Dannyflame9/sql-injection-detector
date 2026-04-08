bba4a283cd5e69e1f050fc23dded50d047b6a9c0
Olanite Daniel Pelumi <136409097+Dannyflame9@users.noreply.github.com>
2026-04-08 12:27:53 +0100

    Initialize SQL Injection Detector package

---
+++ detector/init.py
@@ -0,0 +1,10 @@
+"""
+SQL Injection Detector Package
+Author: Olanite Daniel Pelumi
+"""
+
+from .scanner import SQLiScanner
+
+__version__ = "1.0.0"
+__author__ = "Olanite Daniel Pelumi"
+__all__ = ["SQLiScanner"]
